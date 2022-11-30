6a7d45404c2363ea7ed9f2e4df4e589d1ea92614
sdziuda <szymondziuda95@gmail.com>
2022-11-30 23:13:22 +0100

    Second version
    poprawione parę przeplotów ale możliwe że jest półaktywne oczekiwanie

--- src/cp2022/solution/ConcurrentWorkshop.java
+++ src/cp2022/solution/ConcurrentWorkshop.java
@@ -38,15 +38,10 @@ public class ConcurrentWorkshop implements Workshop {
     public Workplace enter(WorkplaceId wid) {
         try {
             mutex.acquire();
-            Workplace workplace = workplaces.stream().filter(w -> w.getId().equals(wid)).findFirst().orElse(null);
             queueToWorkplace.get(wid).add(Thread.currentThread());
             mutex.release();
-            workplaceSemaphores[workplaces.indexOf(workplace)].acquire();
-            mutex.acquire();
-            workplaceOwners.put(wid, Thread.currentThread().getId());
-            queueToWorkplace.get(wid).remove(Thread.currentThread());
-            mutex.release();
-            return workplace;
+
+            return this.enterIfFirstAndEmpty(wid);
         } catch (InterruptedException e) {
             throw new RuntimeException("panic: unexpected thread interruption");
         }
@@ -54,20 +49,65 @@ public class ConcurrentWorkshop implements Workshop {
 
     @Override
     public Workplace switchTo(WorkplaceId wid) {
-        leave();
-        return enter(wid);
+        if (workplaceOwners.get(wid).equals(Thread.currentThread().getId())) {
+            return this.getWorkplace(wid);
+        } else {
+            try {
+                mutex.acquire();
+                WorkplaceId oldWid = this.getWorkplaceId(Thread.currentThread());
+                workplaceOwners.put(oldWid, 0L);
+                workplaceSemaphores[this.getWorkplaceIndex(oldWid)].release();
+                queueToWorkplace.get(wid).add(Thread.currentThread());
+                mutex.release();
+
+                return this.enterIfFirstAndEmpty(wid);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("panic: unexpected thread interruption");
+            }
+        }
     }
 
     @Override
     public void leave() {
         try {
             mutex.acquire();
-            WorkplaceId wid = Objects.requireNonNull(workplaceOwners.entrySet().stream()
-                    .filter(e -> e.getValue().equals(Thread.currentThread().getId())).findFirst().orElse(null)).getKey();
+            WorkplaceId wid = this.getWorkplaceId(Thread.currentThread());
             workplaceOwners.put(wid, 0L);
+            workplaceSemaphores[this.getWorkplaceIndex(wid)].release();
             mutex.release();
-            workplaceSemaphores[workplaces.indexOf(workplaces.stream().filter(w -> w.getId().equals(wid))
-                    .findFirst().orElse(null))].release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("panic: unexpected thread interruption");
+        }
+    }
+
+    private WorkplaceId getWorkplaceId(Thread thread) {
+        return Objects.requireNonNull(workplaceOwners.entrySet().stream()
+                .filter(e -> e.getValue().equals(thread.getId())).findFirst().orElse(null)).getKey();
+    }
+
+    private int getWorkplaceIndex(WorkplaceId wid) {
+        return workplaces.indexOf(workplaces.stream().filter(w -> w.getId().equals(wid)).findFirst().orElse(null));
+    }
+
+    private Workplace getWorkplace(WorkplaceId wid) {
+        return workplaces.stream().filter(w -> w.getId().equals(wid)).findFirst().orElse(null);
+    }
+
+    private Workplace enterIfFirstAndEmpty(WorkplaceId wid) {
+        try {
+            Workplace workplace = this.getWorkplace(wid);
+            while (true) {
+                mutex.acquire();
+                if (workplaceOwners.get(wid) == 0L && queueToWorkplace.get(wid).peek() == Thread.currentThread()) {
+                    workplaceSemaphores[workplaces.indexOf(workplace)].acquire();
+                    workplaceOwners.put(wid, Thread.currentThread().getId());
+                    queueToWorkplace.get(wid).poll();
+                    mutex.release();
+                    return workplace;
+                } else {
+                    mutex.release();
+                }
+            }
         } catch (InterruptedException e) {
             throw new RuntimeException("panic: unexpected thread interruption");
         }
