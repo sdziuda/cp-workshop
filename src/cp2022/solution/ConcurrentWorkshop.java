3e9e5f702a0a2d42cb497abb99176f31980e4de5
sdziuda <szymondziuda95@gmail.com>
2022-12-04 00:43:58 +0100

    Sixth version
    dodana obsługa cykli ale czasem się deadlockuje a czasem wywala bezpieczeństwo

--- src/cp2022/solution/ConcurrentWorkshop.java
+++ src/cp2022/solution/ConcurrentWorkshop.java
@@ -5,9 +5,7 @@ import cp2022.base.WorkplaceId;
 import cp2022.base.Workshop;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 public class ConcurrentWorkshop implements Workshop {
 
@@ -28,7 +26,7 @@ public class ConcurrentWorkshop implements Workshop {
     }
 
     @Override
-    public Workplace enter(WorkplaceId wid) {
+    public WorkplaceWrapper enter(WorkplaceId wid) {
         this.threadSemaphores.putIfAbsent(Thread.currentThread().getId(), new Semaphore(0));
         WorkplaceWrapper workplaceWrapper = this.getWorkplaceWrapper(wid);
         workplaceWrapper.enter();
@@ -36,8 +34,8 @@ public class ConcurrentWorkshop implements Workshop {
     }
 
     @Override
-    public Workplace switchTo(WorkplaceId wid) {
-        WorkplaceWrapper workplaceFrom = this.getWorkplaceWrapper(Thread.currentThread());
+    public WorkplaceWrapper switchTo(WorkplaceId wid) {
+        WorkplaceWrapper workplaceFrom = this.getWorkplaceWrapper(Thread.currentThread().getId());
         WorkplaceWrapper workplaceTo = this.getWorkplaceWrapper(wid);
         workplaceFrom.switchTo(workplaceTo);
         return workplaceTo;
@@ -45,7 +43,7 @@ public class ConcurrentWorkshop implements Workshop {
 
     @Override
     public void leave() {
-        WorkplaceWrapper workplaceWrapper = this.getWorkplaceWrapper(Thread.currentThread());
+        WorkplaceWrapper workplaceWrapper = this.getWorkplaceWrapper(Thread.currentThread().getId());
         workplaceWrapper.leave();
         threadSemaphores.remove(Thread.currentThread().getId());
     }
@@ -57,9 +55,9 @@ public class ConcurrentWorkshop implements Workshop {
                 .orElseThrow(() -> new RuntimeException("panic: workplace not found"));
     }
 
-    private WorkplaceWrapper getWorkplaceWrapper(Thread t) {
+    private WorkplaceWrapper getWorkplaceWrapper(long threadId) {
         return this.workplaces.stream()
-                .filter(workplaceWrapper -> workplaceWrapper.owner == t.getId())
+                .filter(workplaceWrapper -> workplaceWrapper.owner == threadId)
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("panic: workplace not found"));
     }
@@ -73,6 +71,8 @@ public class ConcurrentWorkshop implements Workshop {
         private final ConcurrentWorkshop workshop;
         private long owner;
         private final Queue<Long> queue;
+        private Queue<WorkplaceId> cycle;
+        private CountDownLatch latch;
 
         public WorkplaceWrapper(Workplace workplace, ConcurrentWorkshop workshop) {
             super(workplace.getId());
@@ -102,11 +102,6 @@ public class ConcurrentWorkshop implements Workshop {
             try {
                 this.workshop.mutex.acquire();
                 if (workplaceTo.getId() == this.getId()) {
-                    this.owner = -1;
-                    this.queue.add(Thread.currentThread().getId());
-                    this.queue.poll();
-                    this.owner = Thread.currentThread().getId();
-                    this.workshop.threadSemaphores.get(this.owner).release();
                     this.workshop.mutex.release();
                 } else if (workplaceTo.owner == -1) {
                     workplaceTo.owner = Thread.currentThread().getId();
@@ -120,15 +115,48 @@ public class ConcurrentWorkshop implements Workshop {
                     int index = this.workshop.getWorkplaceIndex(this);
                     int indexTo = this.workshop.getWorkplaceIndex(workplaceTo);
                     this.workshop.whereToSwitch[index] = indexTo;
-                    workplaceTo.queue.add(Thread.currentThread().getId());
-                    // cycle detection
-                    this.workshop.mutex.release();
-                    this.workshop.threadSemaphores.get(Thread.currentThread().getId()).acquire();
-                    this.workshop.whereToSwitch[index] = -1;
-                    this.owner = -1;
-                    workplaceTo.queue.poll();
-                    workplaceTo.owner = Thread.currentThread().getId();
-                    this.workshop.mutex.release();
+                    boolean cycle = false;
+                    while (this.workshop.whereToSwitch[indexTo] != -1) {
+                        if (this.workshop.whereToSwitch[indexTo] == index) {
+                            cycle = true;
+                            break;
+                        }
+                        indexTo = this.workshop.whereToSwitch[indexTo];
+                    }
+                    if (cycle) {
+                        indexTo = this.workshop.whereToSwitch[index];
+                        var queueCycle = new ConcurrentLinkedDeque<WorkplaceId>();
+                        queueCycle.add(this.getId());
+                        while (indexTo != index) {
+                            queueCycle.add(this.workshop.workplaces.get(indexTo).getId());
+                            indexTo = this.workshop.whereToSwitch[indexTo];
+                        }
+                        Queue<WorkplaceId> reversedQueue = new ConcurrentLinkedQueue<>();
+                        Queue<WorkplaceId> cycleCopy = new ConcurrentLinkedQueue<>(queueCycle);
+                        int size = queueCycle.size();
+                        while (!queueCycle.isEmpty()) {
+                            WorkplaceId id = queueCycle.pollLast();
+                            reversedQueue.add(id);
+                            WorkplaceWrapper workplace = this.workshop.getWorkplaceWrapper(id);
+                            workplace.cycle = cycleCopy;
+                            workplace.latch = new CountDownLatch(size);
+                        }
+                        while (reversedQueue.size() != 1) {
+                            this.workshop.threadSemaphores.get(this.workshop.getWorkplaceWrapper(reversedQueue.poll()).owner).release();
+                        }
+                        this.workshop.whereToSwitch[index] = -1;
+                        workplaceTo.owner = Thread.currentThread().getId();
+                        this.workshop.mutex.release();
+                    } else {
+                        workplaceTo.queue.add(Thread.currentThread().getId());
+                        this.workshop.mutex.release();
+                        this.workshop.threadSemaphores.get(Thread.currentThread().getId()).acquire();
+                        workplaceTo.queue.remove(Thread.currentThread().getId());
+                        this.workshop.whereToSwitch[index] = -1;
+                        workplaceTo.owner = Thread.currentThread().getId();
+                        if (this.owner == Thread.currentThread().getId()) this.owner = -1;
+                        this.workshop.mutex.release();
+                    }
                 }
             } catch (InterruptedException e) {
                 throw new RuntimeException("panic: unexpected thread interruption");
@@ -151,6 +179,23 @@ public class ConcurrentWorkshop implements Workshop {
 
         @Override
         public void use() {
+            if (this.cycle != null) {
+                try {
+                    this.workshop.mutex.acquire();
+                    for (WorkplaceId id : cycle) {
+                        WorkplaceWrapper workplace = this.workshop.getWorkplaceWrapper(id);
+                        var latch = workplace.latch;
+                        latch.countDown();
+                    }
+                    this.latch.await();
+                    this.cycle = null;
+                    this.latch = null;
+                    this.workshop.mutex.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException("panic: unexpected thread interruption");
+                }
+            }
+
             this.workplace.use();
         }
     }
